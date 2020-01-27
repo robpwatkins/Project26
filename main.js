5159189b244b685ce7c4bea342dabe0bc0d582d6
Rob W <robpwatkins@gmail.com>
2020-01-27 17:02:15 -0600

    well i tried

--- main.js
+++ main.js
@@ -1,1 +1,27 @@
-console.log('heyoo');
+const screen = document.querySelector('.screen');
+let screenNums = [];
+let operatorNums = [];
+
+
+
+
+
+document.querySelectorAll('.buttonNum').forEach(button => button.addEventListener('click', clickButtonNum))
+document.querySelectorAll('.buttonOperator').forEach(button => button.addEventListener('click', clickOperator))
+document.querySelector('.equals').addEventListener('click', equals);
+
+
+function clickButtonNum (event) {
+  screenNums.push(event.target.textContent)
+  screen.textContent = screenNums.join('');
+}
+
+function clickOperator () {
+  operatorNums.push(screenNums.join(''));
+  screenNums = [];
+  console.log(operatorNums);
+}
+
+function equals () {
+  console.log(operatorNums);
+}
